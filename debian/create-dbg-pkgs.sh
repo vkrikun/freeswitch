b40dd77d708f7e51131a17d75d495b4de832c53a
Travis Cross <tc@traviscross.com>
2012-04-22 23:46:56 +0000

    Avoid copying twice

--- debian/create-dbg-pkgs.sh
+++ debian/create-dbg-pkgs.sh
@@ -4,6 +4,7 @@ create_dbg_pkgs () {
   for x in debian/*; do
     test ! -d $x && continue
     test "$x" = "tmp" -o "$x" = "source" && continue
+    test ! "$x" = "${x%-dbg}" && continue
     test ! -d $x/usr/lib/debug && continue
     mkdir -p $x-dbg/usr/lib
     mv $x/usr/lib/debug $x-dbg/usr/lib/
